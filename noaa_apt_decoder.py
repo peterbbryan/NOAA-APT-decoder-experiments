cd16dd3b07a620fea4aa1afe41051eaa1fb23bd3
Peter Bryan <peterbbryan@gmail.com>
2020-10-02 16:23:33 -0400

    Making black the project default format

--- noaa_apt_decoder.py
+++ noaa_apt_decoder.py
@@ -9,6 +9,7 @@ from PIL import Image
 from scipy.io import wavfile
 from scipy.signal import hilbert, resample
 
+# fmt: off
 # width of image components after reconstruction
 # see https://www.sigidwiki.com/wiki/Automatic_Picture_Transmission_(APT)
 COMPONENT_SIZES = {
@@ -30,9 +31,12 @@ SYNCHRONIZATION_SEQUENCE = np.array([0, 0, 255, 255, 0, 0, 255, 255,
                                      0, 0, 255, 255, 0, 0, 0, 0, 0,
                                      0, 0, 0]) - 128
 
+# fmt: on
+
 
 def audio_to_hilbert(in_path: str, resample_rate: int = 20800) -> np.ndarray:
-    """ Load the audio and convert to Hilbert transformed amplitude info
+    """
+    Load the audio and convert to Hilbert transformed amplitude info
 
     :param in_path: string to path of audio file, relative or absolute
     :param resample_rate: rate to resample audio at
@@ -57,18 +61,20 @@ def audio_to_hilbert(in_path: str, resample_rate: int = 20800) -> np.ndarray:
 
 
 def subsample(arr: np.ndarray, step: np.ndarray = 5) -> np.ndarray:
-    """ Sampling from the Hilbert transformed signal for desired images
+    """
+    Sampling from the Hilbert transformed signal for desired images
 
     :param arr: signal after Hilbert transform
     :param step: signal after sampling for channels of interest
     :return: subsampled signal
     """
 
-    return resample(arr, len(arr)//step)
+    return resample(arr, len(arr) // step)
 
 
 def signal_to_noise(arr: np.ndarray, axis=0) -> np.ndarray:
-    """ Signal to noise (SNR) calculation (previously included in scipy)
+    """
+    Signal to noise (SNR) calculation (previously included in scipy)
 
     :param arr: array to calculate signal to noise against
     :param axis: axis to perform SNR calculation over
@@ -83,7 +89,8 @@ def signal_to_noise(arr: np.ndarray, axis=0) -> np.ndarray:
 
 
 def quantize(arr: np.ndarray, black_point: int, white_point: int) -> np.ndarray:
-    """ Digitize signal to valid pixel intensities in the uint8 range
+    """
+    Digitize signal to valid pixel intensities in the uint8 range
 
     :param arr: numpy array of continuous signal
     :param black_point: dynamic range lower bound, percent
@@ -101,10 +108,13 @@ def quantize(arr: np.ndarray, black_point: int, white_point: int) -> np.ndarray:
     return arr.astype(np.uint8)
 
 
-def reshape(arr: np.ndarray,
-            synchronization_sequence: np.ndarray = SYNCHRONIZATION_SEQUENCE,
-            minimum_row_separation: int = 2000) -> np.ndarray:
-    """ Reshape the numpy array to a 2D image array
+def reshape(
+    arr: np.ndarray,
+    synchronization_sequence: np.ndarray = SYNCHRONIZATION_SEQUENCE,
+    minimum_row_separation: int = 2000,
+) -> np.ndarray:
+    """
+    Reshape the numpy array to a 2D image array
 
     :param arr: 1D numpy array to arrange into 2D image array
     :param synchronization_sequence: sequence to indicate row start
@@ -119,7 +129,10 @@ def reshape(arr: np.ndarray,
     for current_loc in range(len(arr) - len(synchronization_sequence)):
 
         # proposed start of row, normalized to zero
-        row = [x - 128 for x in arr[current_loc: current_loc + len(synchronization_sequence)]]
+        row = [
+            x - 128
+            for x in arr[current_loc : current_loc + len(synchronization_sequence)]
+        ]
 
         # correlation between the row and the synchronization sequence
         temp_corr = np.dot(synchronization_sequence, row)
@@ -127,19 +140,20 @@ def reshape(arr: np.ndarray,
         # if you're past the minimum separation, start hunting for the next synch
         if current_loc - previous_ind > minimum_row_separation:
             previous_corr, previous_ind = -np.inf, current_loc
-            rows.append(arr[current_loc: current_loc + 2080])
+            rows.append(arr[current_loc : current_loc + 2080])
 
         # if the proposed region matches the sequence better, update
         elif temp_corr > previous_corr:
             previous_corr, previous_ind = temp_corr, current_loc
-            rows[-1] = arr[current_loc: current_loc + 2080]
+            rows[-1] = arr[current_loc : current_loc + 2080]
 
     # stack the row to form the image, drop the incomplete rows at the end
     return np.vstack([row for row in rows if len(row) == 2080])
 
 
 def filter_noisy_rows(arr: np.ndarray) -> np.ndarray:
-    """ Some empirically based filters for noisy rows
+    """
+    Some empirically based filters for noisy rows
 
     :param arr: 2D image array
     :return: image with filtered noisy rows
@@ -151,16 +165,16 @@ def filter_noisy_rows(arr: np.ndarray) -> np.ndarray:
 
     # image filter for rows with high snr (pure noise) and minimal distance
     # in SNR between rows (no theoretical basis, just seems to work)
-    arr = arr[(snr > 0.8) &
-              (snr_diff < 0.05) &
-              (snr_diff > -0.05) &
-              (snr_diff != 0), :]
+    arr = arr[(snr > 0.8) & (snr_diff < 0.05) & (snr_diff > -0.05) & (snr_diff != 0), :]
 
     return arr
 
 
-def select_image_components(arr: np.ndarray, components: Optional[List[str]]) -> np.ndarray:
-    """ Select the image components to include
+def select_image_components(
+    arr: np.ndarray, components: Optional[List[str]]
+) -> np.ndarray:
+    """
+    Select the image components to include
 
     :param arr: 2D image array
     :param components: portions of the image to preserve/filter
@@ -183,7 +197,8 @@ def select_image_components(arr: np.ndarray, components: Optional[List[str]]) ->
 
 
 def save_image(arr: np.ndarray, out_path: str) -> None:
-    """ Write numpy array to image file
+    """
+    Write numpy array to image file
 
     :param arr: 2D image array to write to image
     :param out_path: path to save image
@@ -193,9 +208,12 @@ def save_image(arr: np.ndarray, out_path: str) -> None:
     image.save(out_path)
 
 
-def apply_colormap(arr: np.ndarray,
-                   cm: matplotlib.colors.LinearSegmentedColormap = plt.get_cmap('gist_earth')) -> np.ndarray:
-    """ False colorization based on greyscale intensity
+def apply_colormap(
+    arr: np.ndarray,
+    cm: matplotlib.colors.LinearSegmentedColormap = plt.get_cmap("gist_earth"),
+) -> np.ndarray:
+    """
+    False colorization based on greyscale intensity
 
     :param arr: 2D image array
     :param cm: map between greyscale and colorized
@@ -208,12 +226,17 @@ def apply_colormap(arr: np.ndarray,
     return colorized[:, :, :3] * 255
 
 
-def decode_noaa_audio_to_image(*, in_path: str, out_path: str,
-                               black_point: int = 5,
-                               white_point: int = 95,
-                               components: Optional[List[str]] = None,
-                               colorize: bool = False) -> None:
-    """ Command line interface call for NOAA  apt processing
+def decode_noaa_audio_to_image(
+    *,
+    in_path: str,
+    out_path: str,
+    black_point: int = 5,
+    white_point: int = 95,
+    components: Optional[List[str]] = None,
+    colorize: bool = False,
+) -> None:
+    """
+    Command line interface call for NOAA  apt processing
 
     :param in_path: path to the input audio file
     :param out_path: output path to png
@@ -226,9 +249,10 @@ def decode_noaa_audio_to_image(*, in_path: str, out_path: str,
 
     # components of the image to include
     if components is not None:
-        assert set(components) < set(COMPONENT_SIZES.keys()), \
-            f"Default to all segments when 'components' is None, otherwise all " \
+        assert set(components) < set(COMPONENT_SIZES.keys()), (
+            f"Default to all segments when 'components' is None, otherwise all "
             f"list elements must be in {COMPONENT_SIZES.keys()}"
+        )
 
     # load the audio and convert to Hilbert transformed amplitude info
     decoded = audio_to_hilbert(in_path)
@@ -237,8 +261,7 @@ def decode_noaa_audio_to_image(*, in_path: str, out_path: str,
     subsampled = subsample(decoded)
 
     # digitize signal to valid pixel intensities in the uint8 range
-    quantized = quantize(subsampled, black_point=black_point,
-                         white_point=white_point)
+    quantized = quantize(subsampled, black_point=black_point, white_point=white_point)
 
     # reshape the numpy array to a 2D image array
     reshaped = reshape(quantized)
